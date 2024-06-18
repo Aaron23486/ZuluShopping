346f277b66c1bf48eb9adb1d4e0392e92b5c5430
Aaron234786 <aaron20770@gmail.com>
2024-06-17 19:22:36 -0600

    Video 4 y 5 curso Zulu

---
+++ ZuluShooping/ZuluShooping/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZuluShooping.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
